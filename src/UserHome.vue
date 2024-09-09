47a8548b00e26f105b83c5829ef4adcc545dd4a9
melihguleyupoglu <melihguleyup@gmail.com>
2024-09-09 13:45:09 +0300

    spotify authentication status update

--- src/UserHome.vue
+++ src/UserHome.vue
@@ -19,16 +19,35 @@ const saveUiState = () => {
       class="button sp-button generic-button"
       @click="spotifyStore.authorizeSpotify(), saveUiState()"
       aria-label="Link your spotify account"
+      v-if="!spotifyStore.isSpotifyAuthenticated"
     >
       Link your spotify account
     </button>
     <button
-      class="button sc-button generic-button"
-      @click="spotifyStore.logoutSpotify()"
-      aria-label="Link your soundcloud account"
+      class="button generic-button"
+      v-if="spotifyStore.isSpotifyAuthenticated"
+      @click="spotifyStore.authorizeSpotify(), saveUiState()"
     >
-      Unlink your spotify account
+      Get your stats
     </button>
+  </div>
+  <div class="info-text">
+    <svg
+      class="info-svg"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      style="fill: rgba(0, 0, 0, 1); msfilter:"
+    >
+      <path
+        d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"
+      ></path>
+      <path d="M11 11h2v6h-2zm0-4h2v2h-2z"></path>
+    </svg>
+    <p>
+      To continue with a different account you can log out your spotify account on your used browser
+    </p>
   </div>
 </template>
 <style>
@@ -75,5 +94,10 @@ const saveUiState = () => {
 
 body.light .generic-button:hover {
   outline: 2px solid black;
+}
+
+.info-text {
+  margin-top: 5rem;
+  margin-left: 5rem;
 }
 </style>
