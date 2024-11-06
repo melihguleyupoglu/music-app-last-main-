f49f2c05e2964833934e9bc18d06994a80f95d20
melihguleyupoglu <melihguleyup@gmail.com>
2024-11-06 14:18:56 +0300

    feat(MainNav): button style changes

--- src/MainNav.vue
+++ src/MainNav.vue
@@ -5,39 +5,60 @@
         <router-link class="logo-anchor" to="/">
           <h1 class="logo-header">The Music App</h1>
         </router-link>
-        <input
-          tabindex="0"
-          type="checkbox"
-          id="darkmode-toggle"
-          @click="uiStore.toggleDarkMode()"
-          aria-label="Toggle Dark Mode"
-          :aria-checked="uiStore.isDarkMode"
-          role="switch"
-        />
-        <label
-          for="darkmode-toggle"
-          class="dark-label"
-          aria-hidden="true"
-          tabindex="0"
-          @keydown.space.prevent="triggerToggle"
-          @keydown.enter.prevent="triggerToggle"
-        >
-          <div class="indicator"></div>
-        </label>
-        <button class="generic-button" @click="logout" v-if="authenticated">Logout</button>
+        <div class="ops-subnav">
+          <button class="dark-button" v-on:click="uiStore.toggleDarkMode">
+            <svg
+              v-if="!isDarkMode"
+              class="dark-svg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style="fill: rgba(0, 0, 0, 1)"
+            >
+              <path
+                d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"
+              ></path>
+            </svg>
+            <svg
+              v-if="isDarkMode"
+              class="light-svg"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              style="fill: rgba(0, 0, 0, 1)"
+            >
+              <path
+                d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z"
+              ></path>
+            </svg>
+          </button>
+          <!-- <button
+            class="light-button"
+            v-if="isDarkMode"
+            v-on:click="uiStore.toggleDarkMode"
+          ></button> -->
+          <button class="generic-button" @click="logout" v-if="authenticated">Logout</button>
+        </div>
       </nav>
     </div>
   </body>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watchEffect } from 'vue'
 import { uiStore } from './main'
 import { useRouter } from 'vue-router'
 import { authenticated } from './main'
 
+const isDarkMode = ref(uiStore.isDarkMode)
 const router = useRouter()
 const navbar = ref<HTMLElement | null>(null)
+
+watchEffect(() => {
+  isDarkMode.value = uiStore.isDarkMode
+})
 
 const stickyNavbar = () => {
   if (navbar.value) {
@@ -80,14 +101,21 @@ onUnmounted(() => {
 
 <style scoped>
 .nav {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  padding-top: 1rem;
+  display: grid;
+  grid-template-columns: 75% 25%;
   align-items: center;
+  justify-content: center;
 }
 
 .logo-anchor {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
   text-decoration: none;
+  font-size: x-large;
+  width: 20rem;
 }
 
 .logo-header {
@@ -98,14 +126,12 @@ onUnmounted(() => {
   outline: 2px solid white;
 }
 
-.logo-svg {
-  height: 4rem;
-  width: 4rem;
-}
-
-.logo-svg:hover {
-  outline: 2px solid white;
-  border-radius: 2rem;
+.ops-subnav {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 }
 
 #darkmode-toggle {
@@ -192,5 +218,21 @@ onUnmounted(() => {
   to {
     transform: translateX(0);
   }
+}
+
+.dark-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 2rem;
+  width: 2rem;
+  border-color: white;
+  background-color: white;
+  border-radius: 100px;
+}
+
+.light-svg,
+.dark-svg {
+  size: 1rem;
 }
 </style>
