d463886307d5e300579ed1ff94b4af65233f3cfe
melihguleyupoglu <melihguleyup@gmail.com>
2024-11-08 13:31:29 +0300

    style(MainNav): style changes on subnav buttons.

--- src/MainNav.vue
+++ src/MainNav.vue
@@ -12,7 +12,7 @@
               width="24"
               height="24"
               viewBox="0 0 24 24"
-              style="fill: rgba(0, 0, 0, 1); transform:; msfilter:"
+              style="fill: rgba(0, 0, 0, 1)"
             >
               <path
                 d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"
@@ -239,30 +239,23 @@ onUnmounted(() => {
 }
 
 .dark-button,
-.user-button {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 2rem;
-  width: 2rem;
-  background-color: white;
-  border: none;
-}
-
-.dark-button:hover,
-.user-button:hover,
-.logout-button:hover {
-  outline: 2px solid white;
-}
-
+.user-button,
 .logout-button {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 2rem;
   width: 2rem;
-  border: none;
   background-color: white;
+  fill: white;
+  border: none;
+  border-radius: 2px;
+}
+
+.dark-button:hover,
+.user-button:hover,
+.logout-button:hover {
+  outline: 2px solid white;
 }
 
 .light-svg,
